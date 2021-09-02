487500220881a1a16fb944ea28ad8d29aaee2ce1
Simanta Barua <simanta.barua@yhaoo.com>
2021-09-02 13:11:51 +0600

    error  handle

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,32 +1,54 @@
+
+// getting ID
+const bookList = document.getElementById('book-list');
+const resultCount = document.getElementById('result-count');
+const searchField = document.getElementById('search-text');
+
+//searchBooks
 const searchBooks = () => {
-    toggleSpinner('block')
-    const searchField = document.getElementById('search-text');
     const searchText = searchField.value
+    //check search filed value
+    if (searchText === '') {
+        errorMessage('cannot be empty');
+        return;
+    }
+    //clear message
+    errorMessage('');
+    resultCount.innerText = '';
+    //show spinner
+    toggleSpinner('block');
+    //fetch Book
     fetch(`http://openlibrary.org/search.json?q=${searchText}`)
         .then(res => res.json())
         .then(data => displayBooks(data)
         )
-
 }
-
+//show error message
+const errorMessage = (getError) => {
+    document.getElementById('error-message').innerText = getError;
+}
 
 //spinner toggle
-const toggleSpinner = displayStyle =>{
+const toggleSpinner = displayStyle => {
     const spinner = document.getElementById('spinner').style.display = displayStyle;
-    
-}
 
+}
+//display Book
 const displayBooks = books => {
-    
-    const resultCount = document.getElementById('result-count');
-    resultCount.innerText = `${books.numFound}`
-    const result = books.docs
-    const bookList = document.getElementById('book-list');
-    console.log(books);
-    bookList.textContent = '';
-    result.forEach((book) => {
-        const div = document.createElement('tr');
-        div.innerHTML = ` <td ><img src=" https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" alt="" srcset=""></td>
+    //check books result 
+    if (books.numFound === 0) {
+        errorMessage('No Book Found')
+        bookList.textContent = '';
+    }
+    else {
+        //show found books
+        resultCount.innerText = `(${books.numFound})`
+        //getting books
+        const result = books.docs
+        bookList.textContent = '';
+        result.forEach((book) => {
+            const div = document.createElement('tr');
+            div.innerHTML = ` <td ><img src=" https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" alt="" srcset=""></td>
         <td><a href="">${book.title}</a>
             <li class="list-group text-muted"></li>
             <li class="list-group text-success mt-3 mb-5"> ${book.author_name}</li>
@@ -38,7 +60,8 @@ const displayBooks = books => {
             </div>
         </td>`;
 
-        bookList.appendChild(div);
-    });
+            bookList.appendChild(div);
+        });
+    }
     toggleSpinner('none')
 }
