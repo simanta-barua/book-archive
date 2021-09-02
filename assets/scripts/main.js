7490dc06340b648e735506736ab7569c881b361d
Simanta Barua <simanta.barua@yhaoo.com>
2021-09-02 11:49:26 +0600

    spinner commit

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,24 +1,36 @@
-const loadBooks = () => {
-    fetch('./assets/search.json')
+const searchBooks = () => {
+    toggleSpinner('block')
+    const searchField = document.getElementById('search-text');
+    const searchText = searchField.value
+    fetch(`http://openlibrary.org/search.json?q=${searchText}`)
         .then(res => res.json())
         .then(data => displayBooks(data)
         )
+
 }
-loadBooks();
+
+
+//spinner toggle
+const toggleSpinner = displayStyle =>{
+    const spinner = document.getElementById('spinner').style.display = displayStyle;
+    
+}
 
 const displayBooks = books => {
+    
     const resultCount = document.getElementById('result-count');
     resultCount.innerText = `${books.numFound}`
     const result = books.docs
     const bookList = document.getElementById('book-list');
-
-    result.forEach(book => {
+    console.log(books);
+    bookList.textContent = '';
+    result.forEach((book) => {
         const div = document.createElement('tr');
-        div.innerHTML = ` <td ><img src="./assets/image/1.jpg" alt="" srcset=""></td>
+        div.innerHTML = ` <td ><img src=" https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" alt="" srcset=""></td>
         <td><a href="">${book.title}</a>
-            <li class="list-group text-muted"> ${book.publish_place[0]}</li>
+            <li class="list-group text-muted"></li>
             <li class="list-group text-success mt-3 mb-5"> ${book.author_name}</li>
-            <div class="d-flex justify-content-around  mt-5">
+            <div class="d-flex justify-content-around text-muted mt-5">
                 <li class="list-group ">Year : ${book.first_publish_year}</li>
                 <li class="list-group">Language : ${book.language}</li>
                 <li class="list-group">File</li>
@@ -27,6 +39,6 @@ const displayBooks = books => {
         </td>`;
 
         bookList.appendChild(div);
-        
     });
+    toggleSpinner('none')
 }
